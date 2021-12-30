c26d19ae2f758ae8df733238d27bd6c548705794
AisukaYuki <yubingruo@gmail.com>
2021-12-30 17:00:40 +0800

    Update replace.php

--- utils/replace.php
+++ utils/replace.php
@@ -6,14 +6,16 @@ if(!defined('SYSTEM')) {exit();}
 $output = str_replace("\u0026","&",$output);
 switch ($type) {
 	case 0: // 搜索
-		if ($cache_type != "web") {
+		if ($cache_type == "app") {
 			include (ROOT_PATH."utils/fuck_search.php"); // 搜索结果添加提示
+		} elseif ($cache_type == "web") {
+		    include (ROOT_PATH."utils/fuck_search_web.php");
 		}
 		break;
 	case 1: // playurl
 		break;
 	case 2: // 东南亚APP season
-		$output = str_replace('"vip":0','"vip":1',$output); // 支持漫游 #320
+	    $output = str_replace('"vip":0','"vip":1',$output); // 支持漫游 #320
 		$output = str_replace('"status":13','"status":2',$output); // 13不能缓存
 		include (ROOT_PATH."utils/fuck_sub.php"); // 添加中文字幕
 		break;
