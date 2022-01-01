aa9853aed0ac4f50b229cc64d2392dc546bcf3b5
m1saka <1287708285@qq.com>
2022-01-02 00:42:58 +0800

    Re format 3

--- pgc/player/api/playurl/index.php
+++ pgc/player/api/playurl/index.php
@@ -9,18 +9,18 @@ include (ROOT_PATH."utils/version.php");
 // 加载functions
 include (ROOT_PATH."utils/functions.php");
 if (SAVE_CACHE == 1) {
-	include(ROOT_PATH."utils/functions_cache.php");
+	include (ROOT_PATH."utils/functions_cache.php");
 }
 // 处理用户传入参数
 include (ROOT_PATH."utils/process.php");
 // 设置host
-$host = get_host($type, $cache_type);
+$host = get_host($type,$cache_type);
 // 锁区、web接口、X-From-Biliroaming
 include (ROOT_PATH."utils/lock_area.php");
 // 鉴权、替换access_key、获取缓存
 include (ROOT_PATH."utils/auth.php"); // 鉴权
 if (ACCESS_KEY != "") {
-	include(ROOT_PATH."utils/resign.php"); // 替换access_key
+	include (ROOT_PATH."utils/resign.php"); // 替换access_key
 }
 if (SAVE_CACHE == 1) {
 	get_cache(); // 获取缓存
@@ -33,7 +33,7 @@ if (IP_RESOLVE == 1) {
 // 转发到指定服务器
 $url = $host.$path."?".$query;
 if (IP_RESOLVE == 1) {
-	$output = get_webpage($url, $host, $ip);
+	$output = get_webpage($url,$host,$ip);
 } else {
 	$output = get_webpage($url);
 }
@@ -52,3 +52,4 @@ print($output);
 if (SAVE_CACHE == 1) {
 	write_cache(); // 写入playurl
 }
+?>
