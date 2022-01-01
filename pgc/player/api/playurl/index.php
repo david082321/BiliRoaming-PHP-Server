a3ea2e68528f64f96d185820de0c229651b799dc
m1saka <1287708285@qq.com>
2022-01-02 00:39:07 +0800

    Re format

--- pgc/player/api/playurl/index.php
+++ pgc/player/api/playurl/index.php
@@ -3,24 +3,24 @@
 $type = 1;
 $cache_type = "app";
 // 加载配置
-include($_SERVER['DOCUMENT_ROOT'] . "/config.php");
+include($_SERVER['DOCUMENT_ROOT']."/config.php");
 // 加载版本
-include(ROOT_PATH . "utils/version.php");
+include(ROOT_PATH."utils/version.php");
 // 加载functions
-include(ROOT_PATH . "utils/functions.php");
+include(ROOT_PATH."utils/functions.php");
 if (SAVE_CACHE == 1) {
-	include(ROOT_PATH . "utils/functions_cache.php");
+	include(ROOT_PATH."utils/functions_cache.php");
 }
 // 处理用户传入参数
-include(ROOT_PATH . "utils/process.php");
+include(ROOT_PATH."utils/process.php");
 // 设置host
 $host = get_host($type, $cache_type);
 // 锁区、web接口、X-From-Biliroaming
-include(ROOT_PATH . "utils/lock_area.php");
+include(ROOT_PATH."utils/lock_area.php");
 // 鉴权、替换access_key、获取缓存
-include(ROOT_PATH . "utils/auth.php"); // 鉴权
+include(ROOT_PATH."utils/auth.php"); // 鉴权
 if (ACCESS_KEY != "") {
-	include(ROOT_PATH . "utils/resign.php"); // 替换access_key
+	include(ROOT_PATH."utils/resign.php"); // 替换access_key
 }
 if (SAVE_CACHE == 1) {
 	get_cache(); // 获取缓存
@@ -31,7 +31,7 @@ if (IP_RESOLVE == 1) {
 	$ip = $ips[array_rand($ips)];
 }
 // 转发到指定服务器
-$url = $host . $path . "?" . $query;
+$url = $host.$path."?".$query;
 if (IP_RESOLVE == 1) {
 	$output = get_webpage($url, $host, $ip);
 } else {
@@ -45,7 +45,7 @@ if (TG_NOTIFY == 1) {
 	}
 }
 // 替换内容
-include(ROOT_PATH . "utils/replace.php");
+include(ROOT_PATH."utils/replace.php");
 // 返回内容给用户
 print($output);
 // 写入缓存
