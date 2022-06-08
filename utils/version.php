209122a207f2e36f6384bd137858f9087126c109
david082321 <7046617+david082321@users.noreply.github.com>
2022-06-08 17:52:02 +0800

    Update version.php

--- utils/version.php
+++ utils/version.php
@@ -2,7 +2,7 @@
 // 防止外部破解
 if(!defined('SYSTEM')) {exit();} // 防止外部破解，此行勿改
 
-define('VERSION', '4.2.3');
+define('VERSION', '4.4.4');
 // 加上json的Header
 header('Content-Type: application/json; charset=utf-8');
 // 加上web的Header
