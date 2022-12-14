d7cdcc446916730145d75145eb9276fd27665267
david082321 <7046617+david082321@users.noreply.github.com>
2022-12-14 17:10:58 +0800

    Dump version

--- utils/version.php
+++ utils/version.php
@@ -2,7 +2,7 @@
 // 防止外部破解
 if(!defined('SYSTEM')) {exit();} // 防止外部破解，此行勿改
 
-define('VERSION', '4.5.0');
+define('VERSION', '4.5.1');
 // 加上json的Header
 header('Content-Type: application/json; charset=utf-8');
 // 加上web的Header
