5073340dd8152c34a9bac5a6b355dcef1f36ac23
david082321 <7046617+david082321@users.noreply.github.com>
2023-05-25 11:56:43 +0800

    User Agent

--- utils/version.php
+++ utils/version.php
@@ -3,6 +3,7 @@
 if(!defined('SYSTEM')) {exit();} // 防止外部破解，此行勿改
 
 const VERSION = '4.5.1';
+define('AGENT', "biliroaming-php-server/".VERSION);
 // 加上json的Header
 header('Content-Type: application/json; charset=utf-8');
 // 加上web的Header
